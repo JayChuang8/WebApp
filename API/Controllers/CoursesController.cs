3fb4f2e6528d22128551eb75f5e1a718435e557b
Chuang <Jay.Chuang@oag-bvg.gc.ca>
2021-10-04 12:48:56 -0400

    first commit

---
+++ API/Controllers/CoursesController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CoursesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public CoursesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Course>> GetCourses()
+        {
+            return _context.Courses.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Course> GetCourse(int id)
+        {
+            return _context.Courses.Find();
+        }
+    }
+}
---
+++ API/Entities/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public String CourseName { get; set; }
+
+        public int CourseNum { get; set; }
+    }
+}
