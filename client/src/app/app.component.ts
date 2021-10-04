de65cbc1485d0d19312e20e304a50f4b121876d8
Jay Chuang <56184502+JayChuang8@users.noreply.github.com>
2021-10-04 13:23:24 -0600

    Adding angular application

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Jay\'\s App';
+  courses: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/courses').subscribe(response => {
+      this.courses = response;
+    }, error => {
+      console.log(error);
+    })
+  }
+}
